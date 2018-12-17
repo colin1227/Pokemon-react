d0fee3704c55e542f0141feea8bc446dbc9cd816
Colin <colinmdaniel@gamil.com>
2018-12-17 13:51:41 -0600

    another misspelling

--- src/components/GamePage/index.js
+++ src/components/GamePage/index.js
@@ -43,7 +43,7 @@ import './style.css';
     }
     gatherPokemon = async(e) => {
         try{
-            const data = await fetch("https://git.heroku.com/mysterious-everglades-76630.git/crud/grabPokemon");
+            const data = await fetch("https://mysterious-everglades-76630.herokuapp.com/crud/grabPokemon");
             const parsedResponse = await data.json();
             return parsedResponse;
         }
